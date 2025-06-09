2d6f137bb2f34e835fd9f79c729e375abd00efb7
elm19 <128943656+elm19@users.noreply.github.com>
2025-06-09 19:07:10 +0100

    Update backEndLink.ts with pythonanywhere api

--- apps/web/src/config/backEndLink.ts
+++ apps/web/src/config/backEndLink.ts
@@ -1,2 +1,2 @@
-export const backEndLink = 'http://127.0.0.1:5000/'
+export const backEndLink = 'https://elm19.pythonanywhere.com/'
 export const predictions = [] // Placeholder for predictions data
